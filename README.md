0af298dbb77027bc2d7e45d98a93a45572b95ed5
Rico Castelo <rico.castelo@gmail.com>
2021-02-09 15:15:21 -0800

    Update and comment code

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
+# R2D2 Eyes
+Track an object and move the head.  This will use OpenCV to track an object.  It will then turn the head to track the object.  If no specific object is found, it will do some random looking around.
+
+The head is moved with a motor without an encoder, so at initialization, the head will need to determine where forward is.  It will then track any object.  The head position will not be the same the direction the body will move.  This is to ensure the robot will not run away.
+
+Right now the object it tries to detect is a red object.  In the future it will be a specific color or infrared. I prefer not infrared so the camera can be used for other purposes.
+
+# object_cap.py
+Testing is being done right now with a Raspberry PI 3 B, a Adafruit PCA9685 and a servo to test the detection code.
+
+This is the verify the code can detect and move to the correct position. Then the motor controller will be used to take servo input to move the R2D2 head.
+
 # Install OpenCV 4.1.2 on Raspbian Buster
 
 ## Increase the swap space size
--- object_cap.py
+++ object_cap.py
@@ -2,17 +2,17 @@ import cv2
 import numpy as np
 from adafruit_pca9685 import PCA9685
 
-# For LEDs
+# For I2C setup
+# PCA9685 communicates over I2C
 from board import SCL, SDA
 import busio
 
-# For Servo
+# Setup PCA9685 for Servo control
 from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
-#import adafruit_motor.servo
-#servo = adafruit_motor.servo.Servo(0)
 
 # Create the I2C bus interface
+# and connect it to the PCA9685
 i2c_bus = busio.I2C(SCL, SDA)
 pca = PCA9685(i2c_bus)
 
@@ -20,10 +20,7 @@ pca = PCA9685(i2c_bus)
 # Specific to servo
 pca.frequency = 50
 
-#pwm = PCA9685(0x40)
-#pwm.setPWMFreq(50) # Servo Specific
-#pwm.setServoPosition(0, 90)
-
+# Setup the first webcam found
 cap = cv2.VideoCapture(0)
 
 # Get an initial size for frame
@@ -31,57 +28,72 @@ _, frame = cap.read()
 rows, cols, _ = frame.shape
 
 # Start position of the servo
-position = 90 
+position = 90
+# Set the servo to the start position
 kit.servo[0].angle = position
+
+# Determine what is the center of the screen
 center = int(cols/2)
 x_midpoint = int(cols/2)
 
 while True:
+    # Capture a frame from the webcam
     _, frame = cap.read()
     
-    # Only display mask
+    # Create a display mask
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
     # Red colors
+    # We will search for red mask color
     low_red = np.array([161, 155, 84])
     high_red = np.array([179, 255, 255])
     red_mask = cv2.inRange(hsv_frame, low_red, high_red)
     
     # A contour is an area that has the mask color
+    # This will segragate the frame to only display black and white
+    # where white is the color we selected to look for
     contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     
-    # Sort the contours and put biggest as first
+    # Sort the contours and put biggest white spot as first
     contours = sorted(contours, key=lambda x:cv2.contourArea(x), reverse=True)
     
-    # Create a rectangel around the first largest contour found
+    # Create a rectangle around the first largest contour found
     for cnt in contours:
         # Get the dimensions of the contour
         (x,y,w,h) = cv2.boundingRect(cnt)
     
         # Rectangle created around the contour
-        # frame, top right corner, bottom left corner, color, thickness
+        # frame, top right corner, bottom left corner, color or rectangle, thickness
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
         
         # Mid point of the contour
+        # This is used to point the servo to the mid point of the contour
         x_midpoint = int((x + x + w) / 2)
         
         # Stop on the first (largest) 
         break
     
+    # Draw a line down the center of the object
     cv2.line(frame, (x_midpoint, 0), (x_midpoint, 480), (0, 255, 0), 2)
+    
+    # Display the frame with the rectangle and line
     cv2.imshow("Frame", frame)
+    
+    # Debug to view the segragated area
     #cv2.imshow("contour", red_mask)
     
+    # Stop when ESC is pressed
     key = cv2.waitKey(1)
-    
     if key == 27:
         break
     
-    # MOve servo to keep object in the center
+    # Move servo to keep object in the center
+    # Give a buffer around the center
+    # so it does not continously try to correct
     if x_midpoint < center - 30:
-        position += 1
+        position += 1.5
     elif x_midpoint > center + 30:
-        position -= 1
+        position -= 1.5
     
     # Ensure to not exceeds min and max
     if position > 180:
@@ -89,7 +101,9 @@ while True:
     if position < 0:
         position = 0
     
+    # Move the servo to the new position
     kit.servo[0].angle = position
-    
+
+# Cleanup
 cap.release()
 cv2.destroyAllWindows()
